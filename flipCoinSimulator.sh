e1d989b9536c5a7d108c7d0dba0a9c0acc4aafb0
“dinnu247” <dineshkumar.icon@gmail.com>
2021-03-09 21:28:00 +0530

    [dinesh] added loop flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+headcount=0
+tailcount=0
+while [ $headcount -lt n -a  $tailcount -lt n ]
+do 
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
 echo "head wins"
+headcount=$(( $headcount+1 ))
 else
 echo "tail wins"
+tailcount=$(( $tailcount+1 ))
 fi
-
+done
